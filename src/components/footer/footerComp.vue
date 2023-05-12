f92fe1bc5def21dc2d2a781fe1fa23e080ea8e79
gluke0 <milanesigianluca@gmail.com>
2023-05-12 17:40:37 +0200

    dati html header + footer

--- src/components/footer/footerComp.vue
+++ src/components/footer/footerComp.vue
@@ -17,12 +17,65 @@ export default {
 
 <template>
 
+    <footer>
+
+        <div>
+            <h4 class="text-uppercase">about</h4>
+            <div>Masterstudy is Education WordPress theme featured by Learning Management System (LSM) for online education.</div>
+            <div>Developed by Stylemix Themes.</div>
+            <div>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        </div>
+        <div>
+            <h4>contact</h4>
+            <div>USA, California 20, First Avenue, California</div>
+            <div> Tel.: +1 212  458 300 32</div>
+            <div>Fax.: +1 212 375 24 14</div>
+            <div>info@masterstudy.com</div>
+        </div>
+        <div>
+            <h4>pages</h4>
+            <div>
+                <div>
+                    <li>Blog</li>
+                    <li>Home</li>
+                    <li>Shortcodes</li>
+                </div>
+                <div>
+                    <li>Courses</li>
+                    <li>Membership A...</li>
+                    <li>Typography</li>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h4>blog</h4>
+            <div>
+                <img src="../../assets/images/photo-1517520287167-4bbf64a00d66-75x75.jpeg" alt="">
+                <div>
+                    Our main target is to "Developing Yourself as a Leader"
+                    <span>- August 9, 2018</span>
+                </div>
+            </div>
+            <div>
+                <img src="../../assets/images/photo-1490376840453-5f616fbebe5b-75x75.jpeg" alt="">
+                <div>
+                    Those Other College Expenses You Aren't Thinking About
+                    <span>- June 3, 2015</span>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+
 </template>
 
 
 <style lang="scss" scoped>
 
 @use '../../style/partials/variables';
-
 
 </style>
--- src/components/header/headerComp.vue
+++ src/components/header/headerComp.vue
@@ -17,6 +17,25 @@ export default {
 
 <template>
 
+    <div>
+        <img src="../../assets/images/MasterStudy-1.svg" alt="">
+    </div>
+
+    <div>
+        <li>Courses</li>
+        <li>Course Formats</li>
+        <li>Add Course</li>
+        <li>Pages</li>
+        <li>Demos</li>
+    </div>
+
+    <div>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-twitter"></i>
+    </div>
+
 </template>
 
 
