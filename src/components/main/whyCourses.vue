183183b915aeffa3b9371d234c3aa6d34884ad41
gluke0 <milanesigianluca@gmail.com>
2023-05-14 16:46:02 +0200

    html bozza sez whycourses

--- src/components/main/whyCourses.vue
+++ src/components/main/whyCourses.vue
@@ -17,6 +17,47 @@ export default {
 
 <template>
 
+    <section>
+        <h2>Why my Curses?</h2>
+        <div>
+            <div>
+                <div>
+                    <i class="fa-solid fa-briefcase"></i>
+                </div>
+                <div>
+                    <h3>Entrepreneurship</h3>
+                    <p>Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posueree ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <i class="fa-solid fa-rocket"></i>
+                </div>
+                <div>
+                    <h3>Accelerated learning</h3>
+                    <p>Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posueree ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <i class="fa-regular fa-thumbs-up"></i>
+                </div>
+                <div>
+                    <h3>Productivity</h3>
+                    <p>Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posueree ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <i class="fa-solid fa-gear"></i>
+                </div>
+                <div>
+                    <h3>Life Coaching</h3>
+                    <p>Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posueree ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
+                </div>
+            </div>
+        </div>
+    </section>
     
 </template>
 
