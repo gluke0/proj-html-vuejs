f0c58874b223f2ccc2aeb6efa9524b82553cfade
gluke0 <milanesigianluca@gmail.com>
2023-05-14 16:52:59 +0200

    html bozza sez numbers

--- src/components/main/numbers.vue
+++ src/components/main/numbers.vue
@@ -17,12 +17,58 @@ export default {
 
 <template>
 
-    
+    <section id="numbers">
+        <div>
+            <div>
+                <div>
+                    <i class="fa-solid fa-user-group"></i> 
+                </div>
+                <div>
+                    <span>3195</span>
+                    <span>Online Learner</span>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <i class="fa-solid fa-film"></i> 
+                </div>
+                <div>
+                    <span>23</span>
+                    <span>Video Courses</span>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <i class="fa-regular fa-images"></i> 
+                </div>
+                <div>
+                    <span>437</span>
+                    <span>Slider & Images</span>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <i class="fa-solid fa-thumbs-up"></i> 
+                </div>
+                <div>
+                    <span>1440</span>
+                    <span>Excellent Reviews</span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
 </template>
 
 
 <style lang="scss" scoped>
 
 @use '../../style/partials/variables';
+
+#numbers{
+    background-image: url(../../assets/images/counter-1919x1101.jpg);
+    background-size: contain;
+}
 
 </style>
