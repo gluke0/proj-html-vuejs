b0eb7f55e47fba05a2da00c45e60efaa305bacfa
gluke0 <milanesigianluca@gmail.com>
2023-05-14 17:03:55 +0200

    html bozza sez students

--- src/components/main/students.vue
+++ src/components/main/students.vue
@@ -17,6 +17,27 @@ export default {
 
 <template>
 
+    <section>
+        <h2>What Students Say</h2>
+        <div>
+            <i class="fa-solid fa-arrow-left"></i>
+            <img src="../../assets/images/1-100x100.jpg" alt="Student 1">
+            <img src="../../assets/images/2-100x100.jpg" alt="Student 2">
+            <img src="../../assets/images/4-100x100.jpg" alt="Student 4">
+            <i class="fa-solid fa-arrow-right"></i>
+        </div>
+        <div>
+            <h4>Paints of the Future</h4>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+        </div>
+        <div>
+            "The response to your Masterstudy has been really overwhelming! Those who partecipated in the workshop are spreading the word here on campus and the "buzz" is on. The VP of instruction wants you to come back! Her goal is to have more faculty trained. She also wants to attend a workshop herself. Our President told me Masterstudy needs to be the cornestone of our success program."
+        </div>
+    </section>
     
 </template>
 
