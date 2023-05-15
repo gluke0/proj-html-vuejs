3b83d26559b37ca8ff1717f6ebc3a3424b74cb8c
gluke0 <milanesigianluca@gmail.com>
2023-05-15 17:35:04 +0200

    amazon estetica

--- src/components/main/amazon.vue
+++ src/components/main/amazon.vue
@@ -18,14 +18,16 @@ export default {
 <template>
 
     <section id="amazon">
-        <div>
-            <div>
-                <div> Get My Exclusive Investing Book just for $10.50 </div>
-                <div> Vestibulum imentum eu urna vel cursus. Aenean vehicula quam quam, viate pellentesque urna elementum cursus.</div>
-                <img src="../../assets/images/amazon.png" alt="Order On Amazon Button">
+        <div class="container d-flex pt-5">
+            <div class="text-center side px-4 mt-4">
+                <h2 class="mb-4"> Get My Exclusive Investing Book just for $19.50 </h2>
+                <span class="text"> Vestibulum imentum eu urna vel cursus. Aenean vehicula quam quam, viate pellentesque urna elementum cursus.</span>
+                <div class="mt-4">
+                    <img class="amazon" src="../../assets/images/amazon.png" alt="Order On Amazon Button">
+                </div>
             </div>
-            <div>
-                <img src="../../assets/images/book.png" alt="Book Template Image">
+            <div class="side">
+                <img class="book" src="../../assets/images/book.png" alt="Book Template Image">
             </div>
         </div>
     </section>
@@ -35,11 +37,37 @@ export default {
 
 <style lang="scss" scoped>
 
+@use '../../style/main.scss';
 @use '../../style/partials/variables';
 
 #amazon{
     background-image: url(../../assets/images/slider_1-1917x607-1914x606.jpg);
-    background-size: contain;
+    background-size: cover;
+}
+
+.container{
+    width: 70%;
+
+    .side{
+        width: calc(100% / 2);
+    }
+    h2{
+        color: variables.$buttercup;
+    }
+
+    .text{
+        font-style: italic;
+        font-size: 1.3rem;
+        color: white;
+    }
+
+    .amazon{
+        width: 40%;
+    }
+
+    .book{
+        width: 80%;
+    }
 }
 
 </style>
